5a403cb101a5a9f9bf492d9b286d1dfd17f09b0e
Michael Abernathy <mabernathy87@gmail.com>
2025-10-27 18:01:10 +0000

    Changing porkbun provider from cullenmcdermott/porkbun to marcfrederick/porkbun

--- infra/envs/dev/main.tf
+++ infra/envs/dev/main.tf
@@ -7,8 +7,8 @@ terraform {
       version = ">= 4.47.0, < 5.0.0"
     }
     porkbun = {
-      source  = "cullenmcdermott/porkbun"
-      version = ">= 0.3"
+      source  = "marcfrederick/porkbun"
+      version = ">= 1.3.1"
     }
     random = {
       source  = "hashicorp/random"
